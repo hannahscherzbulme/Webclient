00fb779d3c38a9045c7401973c97b07703df4353
hannahscherzbulme <hannah.scherz@bulme.at>
2022-01-26 22:06:50 +0100

    Projekt fertiggestellt

--- webclient.cpp
+++ webclient.cpp
@@ -1,7 +1,39 @@
 #include "webclient.h"
+#include <thread>
 
 Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+    connect(hostname, &QLineEdit::returnPressed, this, &Webclient::on_send_clicked);
+    m_socket = new QTcpSocket(this);
+    connect(m_socket, &QTcpSocket::connected, this, &Webclient::connected);
+    connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::responce);
+}
+
+void Webclient::connected()
+{
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_hostname + "\r\n\r\n").toLocal8Bit());
+}
+
+void Webclient::responce()
+{
+    output->setText(m_socket->readAll());
+    m_socket->disconnectFromHost();
+}
+
+void Webclient::on_send_clicked()
+{
+    if(!hostname->text().isEmpty())
+    {
+        m_hostname = hostname->text();
+        m_socket->connectToHost(m_hostname, m_port);
+        if(!m_socket->waitForConnected(5000))
+        {
+            output->setText(m_hostname + " - Zeitüberschreitung der Anforderung!");
+            m_socket->reset();
+        }
+    }
+    else
+        output->setText("Bitte geben Sie einen Hostname ein!");
 }
--- webclient.h
+++ webclient.h
@@ -1,14 +1,28 @@
 #ifndef WEBCLIENT_H
 #define WEBCLIENT_H
 
+#include <QAbstractSocket>
+#include <QTcpSocket>
 #include "ui_webclient.h"
 
 class Webclient : public QWidget, private Ui::Webclient
 {
     Q_OBJECT
 
-public:
-    explicit Webclient(QWidget *parent = nullptr);
+    public:
+        explicit Webclient(QWidget *parent = nullptr);
+
+    private slots:
+        void on_send_clicked();
+
+    private:
+        void connected();
+        void responce();
+
+    private:
+        QTcpSocket* m_socket;
+        QString m_hostname;
+        unsigned char m_port = 80;
 };
 
 #endif // WEBCLIENT_H
